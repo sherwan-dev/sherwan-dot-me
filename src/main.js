69de7b599b18a669c8b53a5bfc556e8ea60f3051
sherwan <sherwanuodcs@gmail.com>
2026-02-01 21:56:05 +0100

    add CV download functionality

--- src/main.js
+++ src/main.js
@@ -1,1 +1,9 @@
 // import './style.css'
+function downloadCV() { 
+  const link = document.createElement('a');
+  link.href = 'CV_Sherwan_Mahmood.pdf';
+  link.download = 'CV_Sherwan_Mahmood.pdf'; 
+  link.click();
+}
+
+window.downloadCV = downloadCV;
